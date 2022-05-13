7b17757baf2f47f8987f20088ce0068af706eace
welsondeivid <welsondemigod@gmail.com>
2022-05-13 17:16:57 -0300

    mesa com o gol

--- prototype tatic table/Assets/Scripts/GameManagerScript.cs
+++ prototype tatic table/Assets/Scripts/GameManagerScript.cs
@@ -2,59 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* Essa classe È respons·vel por fazer a transiÁ„o posicionamento-partida, bem como gerir a partida
- ser· nessa classe que chamaremos a funÁ„o da IA*/
+/* Essa classe √© respons√°vel por fazer a transi√ß√£o posicionamento-partida, bem como gerir a partida
+ ser√° nessa classe que chamaremos a fun√ß√£o da IA*/
 public class GameManagerScript : MonoBehaviour
 {
-    [SerializeField] public int maxPlayers = 4; // n˙mero max de jogadores em cada time
+    [SerializeField] public int maxPlayers = 4; // n√∫mero max de jogadores em cada time
     [SerializeField] public Color _matchColor;  // cor transparente das tiles no decorrer da partida
     
-    public bool matchon;                        // booleano que controla se a partida comeÁou ou n„o
+    public bool matchon;                        // booleano que controla se a partida come√ßou ou n√£o
     private int turno = 0;                      // turno da partida
-    
+
+    //NOVO
     private float mDistance;
     private float distance = 0f;
     private Transform mPosition;
 
     public List<PlayerA> teamA = new List<PlayerA>(); // Lista com os jogadores do time A
+    string[] jogadores = {"Final", "Jogador", "Jogador1", "Jogador2", "Jogador3"};
+
+    [SerializeField]
+    public GameObject Gol;
+
     public List<GameObject> teamB = new List<GameObject>(); // Lista com os jogadores do time B
 
-    [SerializeField]
-    public Transform GolA;
-    [SerializeField]
-    public Transform GolB;
-
-    public SpriteRenderer _nrenderer;           // componente reder que ser· usado adiante pra modificar
+    public SpriteRenderer _nrenderer;           // componente reder que ser√° usado adiante pra modificar
                                                 // a cor das tiles
-
+   
 
     // Update is called once per frame
     void Update()
     {
-        // se o time A estiver todo em campo, e o espaÁo houver sido apertado
+        // se o time A estiver todo em campo, e o espa√ßo houver sido apertado
         if (Input.GetKeyDown(KeyCode.Space) && teamA.Count >= maxPlayers)
         {
             matchon = true;
             Match();
             // Debug.Log("Match started"); // debug message
         }
-
-        
     }
 
-    //func respons·vel pela partida, gest„o de turnos e execuÁ„o da IA 
+    //func respons√°vel pela partida, gest√£o de turnos e execu√ß√£o da IA 
     private void Match()
     {
+        // vizinhos estao na lista, percorrendo toda vendo o numero dos jogadores
+
         // se for o turno 0 fazemos o grid ficar transparente
         if (turno == 0)
         {
+            GameObject gol = Instantiate(Gol, new Vector3(15.97f, 3.94f, 0), Quaternion.identity);
+            teamA.Add(gol.GetComponent<PlayerA>());
             InvisibleGrid();
             for (int i = 0; i < teamA.Count; i++)
             {
                 //Debug.Log(teamA[i].transform.position);
 
                 // debug, checando se tinha mais de 1 filho
-                // pois todos j· tem 1, o trinagulo na frente
+                // pois todos j√° tem 1, o trinagulo na frente
                 /*if (teamA[i].transform.childCount > quantFilhosJogador)
                 {
                     Debug.Log("com a bola");
@@ -74,7 +77,7 @@ public class GameManagerScript : MonoBehaviour
 
     public bool FindBall()
     {
-        //procurando se alguÈm tem a bola
+        //procurando se algu√©m tem a bola
         for (int i = 0; i < teamA.Count; i++)
         {
             if (teamA[i].withBall)
@@ -86,7 +89,7 @@ public class GameManagerScript : MonoBehaviour
     }
 
     //NOVO
-    public Transform MenorDistancia(Transform playerPosition, bool tocarPraTras)
+    public Transform MenorDistancia(Transform playerPosition)
     {
         mPosition = null;
         mDistance = 99999f;
@@ -95,27 +98,13 @@ public class GameManagerScript : MonoBehaviour
         {
             if (!teamA[i].withBall)
             {
-                if (!tocarPraTras)
-                {
-                    if (teamA[i].transform.position.x >= playerPosition.position.x) // so pode passar para frente
-                    {
-                        distance = Vector3.Distance(playerPosition.position, teamA[i].transform.position);
-
-                        if (distance < mDistance)
-                        {
-                            Debug.Log(teamA[i].transform.position);
-                            mDistance = distance;
-                            mPosition = teamA[i].transform;
-                        }
-                    }
-                }
-                else
+                if (teamA[i].transform.position.x >= playerPosition.position.x) // so pode passar para frente
                 {
                     distance = Vector3.Distance(playerPosition.position, teamA[i].transform.position);
 
                     if (distance < mDistance)
                     {
-                        Debug.Log(teamA[i].transform.position);
+                        //Debug.Log(teamA[i].transform.position);
                         mDistance = distance;
                         mPosition = teamA[i].transform;
                     }
@@ -127,7 +116,7 @@ public class GameManagerScript : MonoBehaviour
     }
 
     //essa func deixa nossas tiles do grid transparentes pra o campo ser exibido em foco principal
-    //porÈm ainda mantendo a estrutura tabuleiro 
+    //por√©m ainda mantendo a estrutura tabuleiro 
     private void InvisibleGrid() 
     {
         // obtendo um array com todas as tiles que compoem nosso grid
@@ -141,5 +130,5 @@ public class GameManagerScript : MonoBehaviour
             _nrenderer.color = _matchColor;
             // Debug.Log(tiles[i].transform.position); //debug message 
         }
-    } 
+    }
 }
